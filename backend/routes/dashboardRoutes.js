d71bac420ebc5f5cdc7b63e88abdbcf0ffcdfb44
phkmolina1986 <pedro.kuchak@gmail.com>
2025-12-01 15:14:48 -0700

    fix routes

--- backend/routes/dashboardRoutes.js
+++ backend/routes/dashboardRoutes.js
@@ -12,9 +12,9 @@ import { verifyToken, authorizeAdmin } from "../middleware/auth.js";
 const router = express.Router();
 
 router.get("/dashboard/stats/:type", verifyToken, getStats);
-router.get("/dashboard/enrollment/:type", getEnrollment);
-router.get("/dashboard/activities/:type", getActivities);
-router.get("/dashboard/questions/:type", getQuestions);
+router.get("/dashboard/enrollment/:type", verifyToken, getEnrollment);
+router.get("/dashboard/activities/:type", verifyToken, getActivities);
+router.get("/dashboard/questions/:type", verifyToken, getQuestions);
 
 
 export default router;
