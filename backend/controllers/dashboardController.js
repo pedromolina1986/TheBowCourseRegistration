64f10b0831a0814e7841b13b94a34cce0ae86dc9
phkmolina1986 <pedro.kuchak@gmail.com>
2025-12-01 15:50:46 -0700

    Dashboard backend + DB

--- backend/controllers/dashboardController.js
+++ backend/controllers/dashboardController.js
@@ -16,11 +16,24 @@ async function getPool() {
 
 export async function getStats(req, res) {
   try {    
+    const pool = await getPool();
+    
+    // Check base user
+    const statsQuery = await pool.request()
+      .query(`
+        SELECT
+          (SELECT COUNT(1) FROM Student) AS students,
+          (SELECT COUNT(1) FROM Course) AS courses,
+          (SELECT COUNT(1) FROM SubmittedForms) AS forms       
+      `);
+
+    const stats = statsQuery.recordset[0];
+        
     return res.json([
-      { label: "Total Students", value: "200", change: "+12 this month", icon: "Users", colors: "text-blue-300" },
-      { label: "Active Courses", value: "24", change: "+3 this term", icon: "BarChart3", colors: "text-green-300" },
+      { label: "Total Students", value: stats.students, change: "+12 this month", icon: "Users", colors: "text-blue-300" },
+      { label: "Active Courses", value: stats.courses, change: "+3 this term", icon: "BarChart3", colors: "text-green-300" },
       { label: "Programs", value: "3", change: "Diploma, Post-Diploma, Certificate", icon: "GraduationCap", colors: "text-purple-300" },
-      { label: "Pending Forms", value: "8", change: "2 urgent", icon: "Mail", colors: "text-red-300" },
+      { label: "Pending Forms", value: stats.forms, change: "2 urgent", icon: "Mail", colors: "text-red-300" },
     ]);
   } catch (err) {
     console.error("getCourses:", err);
@@ -30,11 +43,36 @@ export async function getStats(req, res) {
 
 export async function getEnrollment(req, res) {
   try {    
-    return res.json([
-      { program: "Software Development - Diploma", duration: "2 years • Winter Term", students: 68 },
-      { program: "Software Development - Post-Diploma", duration: "1 year • Winter Term", students: 45 },
-      { program: "Software Development - Certificate", duration: "6 months • Spring Term", students: 43 },
-    ]);
+
+    const pool = await getPool();
+    
+    // Check base user
+    const enrollmentsQuery = await pool.request()
+      .query(`
+            SELECT
+              c.course_name,
+              t.term_name,
+              COUNT(c.course_name) total_registrations
+            FROM
+              CourseRegistration cr
+              JOIN Course c ON c.course_id = cr.course_id
+              JOIN Term t ON t.term_id = cr.term_id
+            GROUP BY
+              c.course_name, t.term_name
+            ORDER BY
+              total_registrations DESC
+      `);
+
+    const enrollments = enrollmentsQuery.recordset;
+    
+    let enrollmentData = [];
+
+    enrollments.forEach(enr => {
+      enrollmentData.push({ program: enr.course_name, duration: enr.term_name, students: enr.total_registrations });
+    });
+      
+    return res.json(enrollmentData);
+
   } catch (err) {
     console.error("getCourses:", err);
     return res.status(500).json({ error: "Failed to retrieve dashboard enrollments" });
@@ -57,11 +95,37 @@ export async function getActivities(req, res) {
 
 export async function getQuestions(req, res) {
   try {    
-    return res.json([
-      { name: "Emily Rodriguez", program: "Diploma Program", question: "Question about prerequisite courses for Advanced Database course. When will registration open?", time: "3 hours ago", status: "New" },
-      { name: "David Park", program: "Post-Diploma Program", question: "Need clarification on course schedule conflicts between Web Development and Mobile App courses.", time: "1 day ago", status: "Pending" },
-      { name: "Lisa Chen", program: "Certificate Program", question: "Payment deadline approaching but unable to access payment portal. Please assist.", time: "2 days ago", status: "Urgent" },
-    ]);
+
+    const pool = await getPool();
+    
+    // Check base user
+    const formsQuery = await pool.request()
+      .query(`
+            SELECT TOP 3 
+              sf.subject,
+              sf.issue_description,
+              sf.submission_date,
+              sf.status,
+              s.program,
+              s.first_name,
+              s.last_name
+            FROM 
+              SubmittedForms sf
+              JOIN Student s ON s.student_id = sf.student_id
+            ORDER BY 
+              sf.form_id DESC
+      `);
+
+    const forms = formsQuery.recordset;
+    
+    let formsData = [];
+
+    forms.forEach(enr => {
+      formsData.push({ name: `${enr.first_name} ${enr.last_name}`, program: enr.program, question: enr.issue_description, time: enr.submission_date, status: enr.status})
+    });
+      
+    return res.json(formsData);
+        
   } catch (err) {
     console.error("getCourses:", err);
     return res.status(500).json({ error: "Failed to retrieve dashboard activities" });
